b6df5681507a2a9de484c885a6e1f4d24f51cc8c
aksilont <aksilont@gmail.com>
2021-09-15 19:58:54 +0300

    Initial Commit

---
+++ Algorithms and Data Structures/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Algorithms and Data Structures
+//
+//  Created by Aksilont on 15.09.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
