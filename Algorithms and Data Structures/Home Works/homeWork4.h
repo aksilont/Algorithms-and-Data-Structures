d1cc9c1b6936367878337461d666fed82ada79c0
aksilont <aksilont@gmail.com>
2021-09-25 16:52:25 +0300

    Практическое задание №4

---
+++ Algorithms and Data Structures/Home Works/homeWork4.h
@@ -0,0 +1,15 @@
+//
+//  homeWork4.h
+//  Algorithms and Data Structures
+//
+//  Created by Aksilont on 22.09.2021.
+//
+
+#ifndef homeWork4_h
+#define homeWork4_h
+
+#include <stdio.h>
+
+void homeWork4(void);
+
+#endif /* homeWork4_h */
